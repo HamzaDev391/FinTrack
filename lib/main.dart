dfcd36efa5c8d9038c9944de4338e9d9e7b03a48
Muhammad Hamza <hamza.dev391@gmail.com>
2026-08-30 00:32:26 +0500

    feat(app): update application wiring

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'core/database/database_initializer.dart';
 import 'app/app.dart';
 
 void main() {
